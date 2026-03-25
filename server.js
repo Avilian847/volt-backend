6d27f5aaab081b7c74e16f1cdba4d9a5b9446801
Wamagoto <kamaawamagoto@gmail.com>
2026-03-25 19:30:53 +0300

    sample

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 require('dotenv').config();
+const db = require('./config/db');
 
 const app = express();
 
